b7e22bec4e4fac2d058f2cdb3e3f2d29583ecc02
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2024-02-06 16:36:53 +0530

    Update config.js

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,24 +1,11 @@
-// require("dotenv").config();
-// const knex = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//       host : process.env.HOST,
-//       port :process.env.PORT,
-//       password :process.env.PASSWORD,
-//       database :process.env.DATABASE
-
-//     }
-//   })
-//  module.exports=knex; 
-
 require("dotenv").config();
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : "localhost",
-      user :"root",
-      password :"Chhaya@123",
-      database :"hyper_verge"
+      host : process.env.HOST,
+      port :process.env.PORT,
+      password :process.env.PASSWORD,
+      database :process.env.DATABASE
 
     }
   })
